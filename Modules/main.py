f4e6e2e50d51d5e7fabba029c6ba9752937b0c07
Shruti Bakare <shruti.bakare31@gmail.com>
2026-08-20 13:03:07 +0530

    Add files via upload

---
+++ Modules/main.py
@@ -0,0 +1,7 @@
+import calc
+a = 10
+b = 5
+
+print("Addition:",calc.add(a,b))
+print("Substraction:",calc.sub(a,b))
+print("Multiplication:",calc.multiply(a,b))
---
+++ Modules/math.py
@@ -0,0 +1,6 @@
+import math 
+n = int(input("Enter a number:"))
+
+print("Square root=",math.sqrt(n))
+print("Square=",math.pow(n,2))
+print("Factorial=",math.factorial(n))
